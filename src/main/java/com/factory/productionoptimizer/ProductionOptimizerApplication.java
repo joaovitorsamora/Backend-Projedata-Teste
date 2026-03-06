3989a11cee9e3cba1c9b5b54fd638decbeefe104
joaovitorsamora <jvsamora00@gmail.com>
2026-03-06 12:57:51 -0300

    setup: estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/factory/productionoptimizer/ProductionOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.factory.productionoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionOptimizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionOptimizerApplication.class, args);
+    }
+
+}
